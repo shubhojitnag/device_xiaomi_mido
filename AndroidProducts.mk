c7c6150ae99b1612b69b984450c47b701e35b4e4
shubhojitnag <shubhojitnag2009@gmail.com>
2022-10-28 16:44:48 +0530

    mido : Bringup for AEX

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aicp_mido.mk
+    $(LOCAL_DIR)/aosp_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    aicp_mido-user \
-    aicp_mido-userdebug \
-    aicp_mido-eng
+    aosp_mido-user \
+    aosp_mido-userdebug \
+    aosp_mido-eng
